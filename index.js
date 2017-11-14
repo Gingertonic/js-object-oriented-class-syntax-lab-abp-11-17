561a095ab7d05953e8c63f61a450b9aa056c4387
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 19:51:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+class Boardmember {
+  constructor(name, homeState, training){
+    this.name = name,
+    this.homeState = homeState,
+    this.training = training
+  }
+
+  veto() {
+    return `No, I must disagree`
+  }
+}
